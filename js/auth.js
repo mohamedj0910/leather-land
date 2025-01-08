d1e116516b742d0d39dc347c9b15fdc40a8be7b8
mommu09 <mohamedj0910@gmail.com>
2025-01-08 15:04:26 +0530

    Sign up fixed

--- js/auth.js
+++ js/auth.js
@@ -124,3 +124,16 @@ if (loginForm) {
       });
   });
 }
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    window.location.href = '/'
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
